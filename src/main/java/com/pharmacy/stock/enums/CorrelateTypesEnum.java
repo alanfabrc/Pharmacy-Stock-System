7ad38ab3afa5e5e81baf53d32237566cb36638dc
Alan Filho <devaneiosdoalan@gmail.com>
2026-01-26 14:06:50 -0300

    feat(correlates): add CorrelateTypesEnum

---
+++ src/main/java/com/pharmacy/stock/enums/CorrelateTypesEnum.java
@@ -0,0 +1,11 @@
+package com.pharmacy.stock.enums;
+
+public enum CorrelateTypesEnum {
+    PERSONAL_HYGIENE,
+    PREVENTION,
+    DIAGNOSIS,
+    COMPLEMENTARY_TREATMENT,
+    COLLECTIVE_DEFENSE;
+
+
+}
